702fc51cc245a322f893ac3a2db4d7fb898b628e
Jason Butz <736696+jbutz@users.noreply.github.com>
2020-10-04 11:07:43 -0400

    Add Evernote Export Builder

---
+++ src/libs/EvernoteExportBuilder.ts
@@ -0,0 +1,52 @@
+import { DateTime } from 'luxon';
+import { create as createXml, fragment as createXmlFragment } from 'xmlbuilder2';
+import { XMLBuilder } from 'xmlbuilder2/lib/interfaces';
+import { PocketBookmark } from "../models/PocketBookmark";
+import { EvernoteNoteBuilder } from "./EvernoteNoteBuilder";
+
+export class EvernoteExportBuilder {
+    constructor(private readonly evernoteNoteBuilder: EvernoteNoteBuilder) { }
+
+    public buildExport(pocketBookmarks: PocketBookmark[]): string {
+        const exportEl = createXml({ version: '1.0', encoding: 'UTF-8' })
+            .ele('en-export', {
+                'export-date': EvernoteExportBuilder.formatDate(new Date()),
+                application: 'Pocket to Evernote',
+                version: 'Pocket to Evernote'
+            })
+            .dtd({ sysID: 'http://xml.evernote.com/pub/evernote-export3.dtd' });
+
+        pocketBookmarks.forEach((bookmark) => {
+            exportEl.import(this.buildNoteElement(bookmark));
+        });
+
+        return exportEl.end({ prettyPrint: true });
+    }
+
+    private buildNoteElement(pocketBookmark: PocketBookmark): XMLBuilder {
+        const noteEl = createXmlFragment()
+            .ele('note');
+
+        noteEl.ele('title')
+            .txt(pocketBookmark.title)
+
+        noteEl.ele('content').dat(this.evernoteNoteBuilder.build(pocketBookmark));
+
+        noteEl.ele('created').txt(EvernoteExportBuilder.formatDate(pocketBookmark.timeAdded));
+        noteEl.ele('updated').txt(EvernoteExportBuilder.formatDate(pocketBookmark.timeAdded));
+        const noteAttributesEl = noteEl.ele('note-attributes')
+        noteAttributesEl.ele('source').txt('Pocket')
+        noteAttributesEl.ele('source-url').txt(pocketBookmark.href)
+        noteAttributesEl.ele('source-application').txt('Pocket to Evernote')
+
+        pocketBookmark.tags.forEach((tag) => {
+            noteEl.ele('tag').txt(tag);
+        });
+
+        return noteEl;
+    }
+
+    static formatDate(input: Date): string {
+        return DateTime.fromJSDate(input).toUTC().toFormat('yyyyMMdd\'T\'HHmmss\'Z\'');
+    }
+}
