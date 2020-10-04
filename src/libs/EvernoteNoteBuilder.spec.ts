5f5bc41bfdaf962e7802363acc36a9772f4c75a5
Jason Butz <736696+jbutz@users.noreply.github.com>
2020-10-04 11:08:07 -0400

    Update Evernote Note Builder tests to use Snapshots

--- src/libs/EvernoteNoteBuilder.spec.ts
+++ src/libs/EvernoteNoteBuilder.spec.ts
@@ -1,7 +1,5 @@
 import { PocketBookmark } from "../models/PocketBookmark";
 import { EvernoteNoteBuilder } from "./EvernoteNoteBuilder";
-import {promises as fs} from 'fs';
-import path from 'path';
 
 describe('EvernoteNoteBuilder', () => {
     let builder: EvernoteNoteBuilder;
@@ -10,7 +8,6 @@ describe('EvernoteNoteBuilder', () => {
     });
 
     it('builds the xml document', async () => {
-        const expectedXml = await fs.readFile(path.join(__dirname, '..','testing','EvernoteNoteBuilder.output00.xml'), {encoding: 'utf-8'});
         const exampleBookmark = new PocketBookmark({
             href: 'https://example.com',
             title: 'Example Bookmark',
@@ -18,6 +15,6 @@ describe('EvernoteNoteBuilder', () => {
             tags: ''
         });
 
-        expect(builder.build(exampleBookmark)).toEqual(expectedXml);
+        expect(builder.build(exampleBookmark)).toMatchSnapshot();
     })
 });
